df0e621ba08fbca1d44e00df29eb74b7b72af98a
lmurphy1 <lmurphy1@stevens.edu>
2020-02-05 18:58:42 -0500

    basic flask

---
+++ run_hello.py
@@ -0,0 +1,12 @@
+from flask import Flask # create web APIs
+app = Flask(__name__)
+
+# app is a flask instance. .route tells where code becomes available
+# methods are curl calls, same route can do different things depending
+@app.route('/hello', methods=["GET","POST"]) # decorator -- save time, available for import
+def hello_world():
+    return "Hello World"
+
+# if you import this somewhere else, you dont want the flask app running
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
