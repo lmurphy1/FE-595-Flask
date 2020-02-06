a941c9f6a22149cfdb6aec459056688840f8832e
lmurphy1 <34901417+lmurphy1@users.noreply.github.com>
2020-02-05 19:45:12 -0500

    Update run_hello.py

--- run_hello.py
+++ run_hello.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World"
 
+@app.route('/<name>', methods=["GET"]) # 
+def hello_name(name):
+    return f"Hello, {name}"
+
 # if you import this somewhere else, you dont want the flask app running
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
